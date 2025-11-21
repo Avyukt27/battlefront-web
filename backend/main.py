5ccfc2b6f08ecc41093b09b74890f164b57b8fc8
Avyukt27 <avyukt.aggarwal007@gmail.com>
2025-11-21 18:33:24 +0530

    Added .env and created structure for frontend (env is barely used)

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,16 @@
+import os
+
 from core.routes import app
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    _ = load_dotenv()
+
+    SECRET_KEY: str | None = os.getenv("SECRET_KEY")
+    DEBUG: bool = os.getenv("DEBUG", "false") == "true"
+
+    if SECRET_KEY is not None:
+        app.config["SECRET_KEY"] = SECRET_KEY
+    app.config["DEBUG"] = DEBUG
+
+    app.run()
